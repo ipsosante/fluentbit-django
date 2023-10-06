adc2ed9f011557103bd50c9e854cdaba3a6b407b
youenn <youenn.boussard@ipsosante.fr>
2023-10-06 15:46:32 +0200

    django 4 version of fluentbit.

--- fluentbit_django/__init__.py
+++ fluentbit_django/__init__.py
@@ -3,6 +3,14 @@ import fluentbit_python
 from django import get_version
 
 
+def get_raw_uri(request):
+    return '{scheme}://{host}{path}'.format(
+        scheme=request.scheme,
+        host=request._get_raw_host(),
+        path=request.get_full_path(),
+    )
+
+
 class FluentbitDjangoHandler(fluentbit_python.FluentbitHandler):
 
     def format(self, record):
@@ -20,7 +28,7 @@ class FluentbitDjangoHandler(fluentbit_python.FluentbitHandler):
         if request:
             message.update({
                 "django_request_method": record.request.META['REQUEST_METHOD'],
-                "django_request_url": record.request.get_raw_uri(),
+                "django_request_url": get_raw_uri(record.request),
                 "django_request_remote_addr": record.request.META['REMOTE_ADDR'],
             })
 
